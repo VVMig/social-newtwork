fc2f34fe877867a0140cdcec57cdda3bccf150dd
Vova Migay <v.migay@itransition.com>
2021-03-30 17:30:56 +0300

    remove unnecessary import

--- src/packages/components/live-widget/Chat.tsx
+++ src/packages/components/live-widget/Chat.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import { ChatProps } from './interfaces';
 import { Styled } from './styled';
-import { Message } from './Message';
 
 export const Chat = ({ sendIcon }: ChatProps) => {
   return (
     <Styled.Chat>
-      <Styled.MessagesContainer>
-        <Message
-          text="asdasdasdasd"
-          displayName="Vova Migay"
-          date={Date.now()}
-          route="#"
-        />
-      </Styled.MessagesContainer>
+      <Styled.MessagesContainer></Styled.MessagesContainer>
       <Styled.Form>
         <Styled.Input placeholder="Add your comment"></Styled.Input>
         <Styled.Button type="submit">{sendIcon}</Styled.Button>
