f592a2467c996afa7cc10f6934ba9c3ab43a1740
Vova Migay <v.migay@itransition.com>
2021-04-12 12:58:46 +0300

    create default type for string

--- src/app/store/User.ts
+++ src/app/store/User.ts
@@ -1,10 +1,13 @@
-import { types } from 'mobx-state-tree';
+import { ISimpleType, types, IOptionalIType } from 'mobx-state-tree';
 
-export const User = types
-  .model('User', {
-    firstName: types.optional(types.string, ''),
-    lastName: types.optional(types.string, ''),
-    email: types.optional(types.string, ''),
-    verified: types.optional(types.boolean, false),
-  })
-  .actions((self) => ({}));
+const defValueString: IOptionalIType<
+  ISimpleType<string>,
+  [undefined]
+> = types.optional(types.string, '');
+
+export const User = types.model('User', {
+  firstName: defValueString,
+  lastName: defValueString,
+  email: defValueString,
+  verified: types.optional(types.boolean, false),
+});
