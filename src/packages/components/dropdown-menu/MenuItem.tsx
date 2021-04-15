e03b594e2b7c115fd9ab409990cf9957acf5a355
Vova Migay <v.migay@itransition.com>
2021-04-15 19:52:21 +0300

    implement dropdown menu

---
+++ src/packages/components/dropdown-menu/MenuItem.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Styled } from './styled';
+import { Item } from './interfaces';
+
+export const MenuItem = ({ title, link, actionHandler, icon }: Item) => {
+  const clickHandler: React.MouseEventHandler = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    actionHandler && actionHandler(event);
+  };
+
+  return (
+    <Styled.MenuItem onClickCapture={clickHandler}>
+      {link ? (
+        <Styled.Link to={link}>
+          {icon}
+          <Styled.Title>{title}</Styled.Title>
+        </Styled.Link>
+      ) : (
+        <>
+          {icon}
+          <Styled.Title>{title}</Styled.Title>
+        </>
+      )}
+    </Styled.MenuItem>
+  );
+};
