c983717f16680f31501de8b8fa1342c2fd32d935
Vova Migay <v.migay@itransition.com>
2021-05-16 15:55:08 +0300

    add send message function

---
+++ src/app/helpers/sendMessage.ts
@@ -0,0 +1,11 @@
+import Axios from 'axios';
+import { axiosConfig } from '../utils/axiosConfig';
+
+const apiClient = Axios.create(axiosConfig);
+
+export const sendMessage = async (text: string, commonRoomId: string) => {
+  await apiClient.post('/user/sendMessage', {
+    text,
+    roomId: commonRoomId,
+  });
+};
